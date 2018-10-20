f1bce0e14d61e8ca8e2434831d8f0590b6931495
Amit Seal Ami <aami@email.wm.edu>
2018-10-19 20:34:58 -0400

    removed comments

--- code/Muse/src/edu/wm/cs/muse/utility/Utility.java
+++ code/Muse/src/edu/wm/cs/muse/utility/Utility.java
@@ -7,23 +7,4 @@ import java.io.IOException;
 
 public class Utility {
 	public static int COUNTER_GLOBAL=0;
-	
-//	/**
-//	 * reads a text file using and returns the contents
-//	 * @param filePath of the file
-//	 * @return StringBuffer that contains the text contents of file
-//	 * @throws FileNotFoundException
-//	 * @throws IOException
-//	 */
-//	public static StringBuffer readSourceFile(String filePath) throws FileNotFoundException, IOException {
-//		StringBuffer source = new StringBuffer();
-//		BufferedReader reader = new BufferedReader(new FileReader(filePath));
-//		String line = null;
-//
-//		while ((line = reader.readLine()) != null) {
-//			source.append(line).append("\n");
-//		}
-//		reader.close();
-//		return source;
-//	}
 }
