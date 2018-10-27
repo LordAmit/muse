fc8bf21304fbfde8f8176c06761255cee31bdb1d
Yang Zhang <yzhang51@email.wm.edu>
2018-10-27 11:55:44 -0400

    taint scheme init

---
+++ code/Muse/src/edu/wm/cs/muse/taint/TaintSchema.java
@@ -0,0 +1,15 @@
+package edu.wm.cs.muse.taint;
+
+public class TaintSchema {
+	
+	/**
+	 * The TaintSchema will traverse the nodes of the AST, and when it reaches a method, it will locate
+	 * the class(es) the method is in and insert a declaration in the class(es) and a source in the method.
+	 * Once the schema has completed its course, it will call SinkOperator to insert sinks that reflect
+	 * the location of the declarations.
+	 * @author yang
+	 */
+	
+	
+
+}
