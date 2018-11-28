6899a2d7afaa2455690ff13eb5f6ff2cbb690260
Yang Zhang <yzhang51@email.wm.edu>
2018-11-28 16:04:59 -0500

    created input files for manual modification

--- code/Muse/src/log/LogAnalyzer.java
+++ code/Muse/src/log/LogAnalyzer.java
@@ -1,8 +1,11 @@
 package log;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
+
+import edu.wm.cs.muse.dataleak.support.FileUtility;
 
 /**
  * Log Analyzer helps to detect and prepare Source files only containing true
@@ -12,33 +15,10 @@ import java.util.Set;
  *
  */
 public class LogAnalyzer {
-	static String testString = "11-28 16:31:53.569 18932-18932/com.example.amit.helloworld D/leak-18: GMT+00:00\n"
-			+ "11-28 16:31:53.599 18932-18932/com.example.amit.helloworld D/leak-22: GMT+00:00\n"
-			+ "11-28 16:32:11.114 18932-18932/com.example.amit.helloworld D/leak-21: GMT+00:00\n"
-			+ "11-28 16:32:11.114 18932-18932/com.example.amit.helloworld D/leak-19: GMT+00:00\n"
-			+ "11-28 16:32:12.745 18932-18932/com.example.amit.helloworld D/leak-21: GMT+00:00\n"
-			+ "11-28 16:32:12.745 18932-18932/com.example.amit.helloworld D/leak-19: GMT+00:00\n";
-	static String sourceString = "package com.example.amit.helloworld;\n" + "\n"
-			+ "import android.support.v7.app.AppCompatActivity;\n" + "import android.os.Bundle;\n"
-			+ "import android.view.View;\n" + "import android.widget.Button;\n" + "\n"
-			+ "public class MainActivity extends AppCompatActivity {\n" + "\n"
-			+ "    String dataLeAk18 = java.util.Calendar.getInstance().getTimeZone().getDisplayName();\n"
-			+ "    Object throwawayLeAk18 = android.util.Log.d(\"leak-18\", dataLeAk18);\n" + "\n"
-			+ "    int intA = 0;\n" + "\n" + "    public int methodA(View v) {\n"
-			+ "        String dataLeAk19 = java.util.Calendar.getInstance().getTimeZone().getDisplayName();\n"
-			+ "        Object throwawayLeAk19 = android.util.Log.d(\"leak-19\", dataLeAk19);\n" + "        intA = 2;\n"
-			+ "        return 1;\n" + "    }\n" + "\n" + "    int intB = 0;\n" + "\n" + "    int methodB() {\n"
-			+ "        String dataLeAk20 = java.util.Calendar.getInstance().getTimeZone().getDisplayName();\n"
-			+ "        Object throwawayLeAk20 = android.util.Log.d(\"leak-20\", dataLeAk20);\n" + "        intB = 3;\n"
-			+ "        return 0;\n" + "    }\n" + "\n" + "    public void button_click(View view) {\n"
-			+ "        String dataLeAk21 = java.util.Calendar.getInstance().getTimeZone().getDisplayName();\n"
-			+ "        Object throwawayLeAk21 = android.util.Log.d(\"leak-21\", dataLeAk21);\n"
-			+ "        android.util.Log.d(\"hey\", \"hello\");\n" + "        methodA(view);\n" + "    }\n" + "\n"
-			+ "    @Override\n" + "    protected void onCreate(Bundle savedInstanceState) {\n"
-			+ "        super.onCreate(savedInstanceState);\n"
-			+ "        String dataLeAk22 = java.util.Calendar.getInstance().getTimeZone().getDisplayName();\n"
-			+ "        Object throwawayLeAk22 = android.util.Log.d(\"leak-22\", dataLeAk22);\n"
-			+ "        setContentView(R.layout.activity_main);\n" + "    }\n" + "}\n";
+	//RuntimeLogs.txt
+	static String testString;
+	//ModifiedFile.txt
+	static String sourceString;
 
 	/**
 	 * Extracts indices of true positive data leaks
@@ -90,7 +70,9 @@ public class LogAnalyzer {
 		return outputLines;
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException, IOException {
+		testString = FileUtility.readSourceFile("src/log/RuntimeLogs.txt").toString();
+		sourceString = FileUtility.readSourceFile("src/log/ModifiedFile.txt").toString();
 		LogAnalyzer log = new LogAnalyzer();
 
 		System.out.println(log.removeUnusedIndicesFromSource(LogAnalyzer.sourceString,
