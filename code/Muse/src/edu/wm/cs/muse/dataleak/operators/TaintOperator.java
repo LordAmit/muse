99c31ae8495b3cf9e17ec3583b78e340ad1f057d
Amit Seal Ami <aami@email.wm.edu>
2019-01-20 13:21:24 -0500

    handles overriding methods so that super is always at top based on android convention

--- code/Muse/src/edu/wm/cs/muse/dataleak/operators/TaintOperator.java
+++ code/Muse/src/edu/wm/cs/muse/dataleak/operators/TaintOperator.java
@@ -3,6 +3,7 @@ package edu.wm.cs.muse.dataleak.operators;
 import java.util.ArrayList;
 
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
 import org.eclipse.jdt.core.dom.Statement;
 import org.eclipse.jdt.core.dom.rewrite.ASTRewrite;
@@ -10,6 +11,7 @@ import org.eclipse.jdt.core.dom.rewrite.ListRewrite;
 
 import edu.wm.cs.muse.dataleak.support.Utility;
 import edu.wm.cs.muse.dataleak.support.node_containers.SourceNodeChangeContainers;
+import edu.wm.cs.muse.dataleak.support.node_containers.SourceNodeChangeContainers.INSERTION_TYPE;
 
 /**
  * The TaintOperator class has two methods, insertDeclaration and insertSource,
@@ -30,41 +32,51 @@ public class TaintOperator {
 	}
 
 	/**
-	 * Modifies the ASTRewrite to insert a taint declaration and a source string
-	 * in the method, then returns it.
+	 * Modifies the ASTRewrite to insert a taint declaration and a source string in
+	 * the method, then returns it.
 	 * 
 	 * @return
 	 */
 	public ASTRewrite InsertChanges() {
 
 		for (SourceNodeChangeContainers nodeChange : nodeChanges) {
-			if (nodeChange.insertionType == 0) {
-				insertion(nodeChange.node, nodeChange.index, nodeChange.propertyDescriptor);
+			// if (nodeChange.insertionType == 0 && nodeChange.node != null ) {
+			if (nodeChange.type == INSERTION_TYPE.METHOD_BODY && nodeChange.node != null) {
+				insertInMethodBody((Block) nodeChange.node, nodeChange.index, nodeChange.propertyDescriptor);
 			} else {
-				insertVariable(nodeChange.node, nodeChange.index, nodeChange.propertyDescriptor);
+				if (nodeChange.node != null)
+					insertVariableDeclaration(nodeChange.node, nodeChange.index, nodeChange.propertyDescriptor);
 			}
 		}
-		
+
 		return rewriter;
 
 	}
 
 	// for inserting source inside methodBody
-	public void insertion(ASTNode node, int index, ChildListPropertyDescriptor nodeProperty) {
+	public void insertInMethodBody(Block node, int index, ChildListPropertyDescriptor nodeProperty) {
+		int placement = 0;
+		for (Object obj : node.statements()) {
+			if (obj.toString().startsWith("super") || obj.toString().startsWith("this(")) {
+				placement++;
+			}
+		}
 		ListRewrite listRewrite = rewriter.getListRewrite(node, nodeProperty);
-		String source = String.format("dataLeAk%d = java.util.Calendar.getInstance().getTimeZone().getDisplayName();", Utility.COUNTER_GLOBAL - 1);
+		String source = String.format("dataLeAk%d = java.util.Calendar.getInstance().getTimeZone().getDisplayName();",
+				Utility.COUNTER_GLOBAL - 1);
 		Statement placeHolder = (Statement) rewriter.createStringPlaceholder(source, ASTNode.EMPTY_STATEMENT);
-		//listRewrite.insertAt(placeHolder, index, null);
-		listRewrite.insertAt(placeHolder, 0, null);
+		// listRewrite.insertAt(placeHolder, index, null);
+		listRewrite.insertAt(placeHolder, placement, null);
 	}
 
 	// for declaration.
-	private void insertVariable(ASTNode node, int index, ChildListPropertyDescriptor nodeProperty) {
+	private void insertVariableDeclaration(ASTNode node, int index, ChildListPropertyDescriptor nodeProperty) {
+	
 		ListRewrite listRewrite = rewriter.getListRewrite(node, nodeProperty);
 		String variable = String.format("String dataLeAk%d = \"%d\";", Utility.COUNTER_GLOBAL, Utility.COUNTER_GLOBAL);
 		Statement placeHolder = (Statement) rewriter.createStringPlaceholder(variable, ASTNode.EMPTY_STATEMENT);
 		listRewrite.insertAt(placeHolder, index, null);
 		Utility.COUNTER_GLOBAL++;
 	}
-	
+
 }
--- code/Muse/src/edu/wm/cs/muse/dataleak/operators/TaintSinkOperator.java
+++ code/Muse/src/edu/wm/cs/muse/dataleak/operators/TaintSinkOperator.java
@@ -2,6 +2,7 @@ package edu.wm.cs.muse.dataleak.operators;
 
 import java.util.ArrayList;
 import org.eclipse.jdt.core.dom.ASTNode;
+import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.ChildListPropertyDescriptor;
 import org.eclipse.jdt.core.dom.FieldDeclaration;
 import org.eclipse.jdt.core.dom.Statement;
@@ -44,7 +45,7 @@ public class TaintSinkOperator {
 		for (TaintNodeChangeContainers fieldChanges : fieldChanges) {
 			for (SinkNodeChangeContainers methodChanges : methodChanges) {
 				if (methodChanges.node == fieldChanges.node) {
-					insertSink(methodChanges.method, methodChanges.index, fieldChanges.fieldBoys,
+					insertSink((Block) methodChanges.method, methodChanges.index, fieldChanges.fieldBoys,
 							methodChanges.propertyDescriptor);
 				}
 			}
@@ -54,11 +55,18 @@ public class TaintSinkOperator {
 	}
 
 	// for sink insertion
-	void insertSink(ASTNode node, int index, ArrayList<FieldDeclaration> fieldBoys,
+	void insertSink(Block node, int index, ArrayList<FieldDeclaration> fieldBoys,
 			ChildListPropertyDescriptor nodeProperty) {
+		ListRewrite listRewrite = null;
+		if (node == null)
+			return;
 		for (int i = 0; i < fieldBoys.size(); i++) {
-
-			ListRewrite listRewrite = rewriter.getListRewrite(node, nodeProperty);
+			try {
+				listRewrite = rewriter.getListRewrite(node, nodeProperty);
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.exit(0);
+			}
 			int index_equal = fieldBoys.get(i).toString().indexOf("=");
 			String tempString = fieldBoys.get(i).toString().substring(15, index_equal);
 			tempString = tempString.trim();
@@ -66,8 +74,13 @@ public class TaintSinkOperator {
 			String sink = String.format("android.util.Log.d(\"leak-%s-%s\", dataLeAk%s);", tempString, index,
 					tempString);
 			Statement placeHolder = (Statement) rewriter.createStringPlaceholder(sink, ASTNode.EMPTY_STATEMENT);
-
-			listRewrite.insertAt(placeHolder, 1, null);
+			int placement = 1;
+			for (Object obj : node.statements()) {
+				if (obj.toString().startsWith("super") || obj.toString().startsWith("this(")) {
+					placement++;
+				}
+			}
+			listRewrite.insertAt(placeHolder, placement, null);
 		}
 
 	}
