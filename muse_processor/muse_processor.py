5b2cf2a48a115a5fed4feffd815cb63a42dce12c
Amit Seal Ami <aami@email.wm.edu>
2019-04-03 10:42:41 -0400

    changed gradle build to gradle assembleDebug to ensure apk is signed

--- muse_processor/muse_processor.py
+++ muse_processor/muse_processor.py
@@ -5,7 +5,6 @@ from pathlib import Path
 from typing import Tuple, List
 import os
 
-
 sh_file_paths: List[str] = []
 mutation_folder_paths: List[str] = []
 path_to_muse_jar: str = "/home/amit/workspaces/muse/Muse.jar"
@@ -51,33 +50,37 @@ def make_folder_structure(project_name: str):
 
     global sh_file_paths
     global mutation_folder_paths
-    create_dir(output_dir+project_name)
-    create_dir(output_dir+project_name+"/sh_files")
-    create_dir(output_dir+project_name+"/schemas")
+    create_dir(output_dir + project_name)
+    create_dir(output_dir + project_name + "/sh_files")
+    create_dir(output_dir + project_name + "/schemas")
 
     for mutation_type in mutation_types:
-        create_dir(output_dir+project_name+"/schemas/"+mutation_type)
+        create_dir(output_dir + project_name + "/schemas/" + mutation_type)
 
 
-def content_in_sh_per_schema(project: str, project_name: str, output_dir: str, mutation: str):
+def content_in_sh_per_schema(project: str, project_name: str, output_dir: str,
+                             mutation: str):
 
     template_schema_apply = """#!/bin/sh
 java -jar {} /home/amit/workspaces/muse/libs4ast {} {} {}{} {} > {}_{}.log
 echo finished executing {}
     """
-    return template_schema_apply.format(path_to_muse_jar, project, project_name,
-                                        output_dir+project_name+"/schemas/", mutation, mutation, project_name, mutation, mutation)
+    return template_schema_apply.format(
+        path_to_muse_jar, project, project_name,
+        output_dir + project_name + "/schemas/", mutation, mutation,
+        project_name, mutation, mutation)
 
 
-def content_in_all_schema_sh_file(project_name: str, project_path: str, output_dir: str):
+def content_in_all_schema_sh_file(project_name: str, project_path: str,
+                                  output_dir: str):
     sh_file_path = output_dir+project_name + \
         "/sh_files/"+"all_schema"+".sh"
     make_sh_file_with_x_bit(sh_file_path)
     sh_clean_project_folder(project_path)
     temp_string: str = "sh clean_project_folder.sh\n"
     for mutation in mutation_types:
-        temp_string += "echo "+project_name+" "+mutation+"\n"
-        temp_string += "sh "+project_name+"_"+mutation+".sh\n"
+        temp_string += "echo " + project_name + " " + mutation + "\n"
+        temp_string += "sh " + project_name + "_" + mutation + ".sh\n"
     open(sh_file_path, mode="w").write(temp_string)
 
 
@@ -98,7 +101,7 @@ def sh_clean_project_folder(project_path: str):
     sh_file_path = output_dir+project_name + \
         "/sh_files/"+"clean_project_folder"+".sh"
     make_sh_file_with_x_bit(sh_file_path)
-    project_gradlew_path = project_path+"gradlew"
+    project_gradlew_path = project_path + "gradlew"
     os.chmod(project_gradlew_path, 0o755)
     temp_string = "#!/bin/bash\ncd {}\n{}gradlew clean".format(
         project_path, project_path)
@@ -112,7 +115,7 @@ def clean_and_build_all(project_name: str, output_dir: str):
     make_sh_file_with_x_bit(sh_file_path)
     temp_string: str = "cd {}\n"
     temp_string += "{} clean\n"
-    temp_string += "{} build\n"
+    temp_string += "{} assembleDebug\n"
     content: str = ""
     for mutation in mutation_types:
         path_project = output_dir+project_name + \
@@ -131,8 +134,8 @@ if __name__ == "__main__":
     for project_name, project_path in name_inputFolders:
         make_folder_structure(project_name)
         for mutation in mutation_types:
-            output = content_in_sh_per_schema(
-                project_path, project_name, output_dir, mutation)
+            output = content_in_sh_per_schema(project_path, project_name,
+                                              output_dir, mutation)
             sh_file_path = output_dir+project_name + \
                 "/sh_files/"+project_name+"_"+mutation+".sh"
 
