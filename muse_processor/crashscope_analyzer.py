bd2500f3c979fdec941981f591ca4db84f39b401
Amit Seal Ami <aami@email.wm.edu>
2019-08-01 20:56:25 -0400

    added support for different format of log files

--- muse_processor/crashscope_analyzer.py
+++ muse_processor/crashscope_analyzer.py
@@ -1,12 +1,16 @@
 # used to find only the executed logs from crashscope and list only the unique ones.
 import sys
 from typing import List, Set
+import custom_log as log
+
+# log.disable()
 
 
 def process_file(filename_crashscope: str, filename_leak_log: str):
     count_yes: int = 0
     count_no: int = 0
     whole_leak_count: int = 0
+    log.log("starting program")
     print("filename: " + filename_crashscope)
     leak_set: Set[str] = set()
     # with open(filename)
@@ -15,7 +19,9 @@ def process_file(filename_crashscope: str, filename_leak_log: str):
         line = line.lower()
         if "leak-" in line:
             whole_leak_count += 1
+            log.log(line)
             leak_substr: str = line.split("/leak-")[1].split("(")[0].strip()
+            log.log("leak_substr: "+leak_substr)
             leak_set.add(leak_substr)
     contents = open(filename_leak_log).read()
     for item in leak_set:
@@ -27,8 +33,44 @@ def process_file(filename_crashscope: str, filename_leak_log: str):
             count_no += 1
             print(str_to_check + ": no")
     print("total leaks:", whole_leak_count, "unique leaks:", len(leak_set),
-          "yes:", count_yes, "no:", count_no)
+          "unique leaks in both logs:", count_yes, "anomalous leak count:",
+          count_no)
+
+
+def process_file2(filename_crashscope: str, filename_leak_log: str):
+    count_yes: int = 0
+    count_no: int = 0
+    whole_leak_count: int = 0
+    log.log("starting program")
+    print("filename: " + filename_crashscope)
+    leak_set: Set[str] = set()
+    # with open(filename)
+    lines: List[str] = open(filename_crashscope).readlines()
+    for line in lines:
+        line = line.lower()
+        if "leak-" in line:
+            whole_leak_count += 1
+            log.log(line)
+            leak_substr: str = line.split("leak-")[1].split(":")[0].strip()
+            log.log("leak_substr: " + leak_substr)
+            leak_set.add(leak_substr)
+    contents = open(filename_leak_log).read()
+    for item in leak_set:
+        str_to_check = "leak-" + item
+        if str_to_check.lower() in contents.lower():
+            print(str_to_check + ": yes")
+            count_yes += 1
+        else:
+            count_no += 1
+            print(str_to_check + ": no")
+    print("total leaks:", whole_leak_count, "unique leaks:", len(leak_set),
+          "unique leaks in both logs:", count_yes, "anomalous leak count:",
+          count_no)
 
 
 if len(sys.argv) == 3:
+    log.log("two arguments provided")
     process_file(sys.argv[1], sys.argv[2])
+elif len(sys.argv) == 4:
+    log.log("three arguments provided")
+    process_file2(sys.argv[1], sys.argv[2])
