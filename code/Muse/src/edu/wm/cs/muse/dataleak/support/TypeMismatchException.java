2c216168ba430a0ab5c3228a2d3abcc7bcc04ca6
Ian Spencer Wolff <ianw.inbox@gmail.com>
2019-10-24 09:41:38 -0400

    Added documentation for TypeMismatchException

--- code/Muse/src/edu/wm/cs/muse/dataleak/support/TypeMismatchException.java
+++ code/Muse/src/edu/wm/cs/muse/dataleak/support/TypeMismatchException.java
@@ -2,17 +2,18 @@ package edu.wm.cs.muse.dataleak.support;
 
 public class TypeMismatchException extends Exception {
 	
-    /**
-	 * 
+	/**
+     * Exception thrown after a type mismatch for ASTNodes
+     * 
+	 * Author: Ian Wolff
 	 */
-	private static final long serialVersionUID = 842241097234226782L;
-
+	private static final long serialVersionUID = 1L;
+	
 	public TypeMismatchException() {
     	super();
     }
 
-    public TypeMismatchException(String message)
-    {
+    public TypeMismatchException(String message) {
        super(message);
     }
 }
