14910d9d5782604e3d0b844a0d1fab43d3ed33cf
Ian Spencer Wolff <ianw.inbox@gmail.com>
2019-10-24 09:44:04 -0400

    Fixed spacing

--- code/Muse/src/edu/wm/cs/muse/dataleak/support/TypeMismatchException.java
+++ code/Muse/src/edu/wm/cs/muse/dataleak/support/TypeMismatchException.java
@@ -2,18 +2,18 @@ package edu.wm.cs.muse.dataleak.support;
 
 public class TypeMismatchException extends Exception {
 	
-	/**
-     * Exception thrown after a type mismatch for ASTNodes
-     * 
-	 * Author: Ian Wolff
-	 */
-	private static final long serialVersionUID = 1L;
-	
-	public TypeMismatchException() {
-    	super();
-    }
+  /**
+  * Exception thrown after a type mismatch for ASTNodes
+  * 
+  * Author: Ian Wolff
+  */
+  private static final long serialVersionUID = 1L;
+  
+  public TypeMismatchException() {
+    super();
+  }
 
-    public TypeMismatchException(String message) {
-       super(message);
-    }
+  public TypeMismatchException(String message) {
+    super(message);
+  }
 }
