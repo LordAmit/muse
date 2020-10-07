c2ebd20031be5b49ffbb63160aa56dcf10750e15
cortrightkevin <cortrightkevin@gmail.com>
2020-10-06 23:08:10 -0400

    Created rewrite method to accept more general form strings.

--- code/Muse/src/edu/wm/cs/muse/dataleak/operators/TryCatchHandler.java
+++ code/Muse/src/edu/wm/cs/muse/dataleak/operators/TryCatchHandler.java
@@ -1,10 +1,13 @@
 package edu.wm.cs.muse.dataleak.operators;
 
 
+import java.lang.reflect.Constructor;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
+
 
 import org.eclipse.jdt.core.dom.Block;
 import org.eclipse.jdt.core.dom.Statement;
@@ -39,6 +42,50 @@ public class TryCatchHandler {
 			}
 		}
 		return false;
+	}
+	
+	/**
+	 * Method that returns true if the inserted string contains a method that contains an exception.
+	 * Takes in a string and parses it to get only the parts needed for checking if an exception is
+	 * thrown, the class name and package, and the methods called within that package. This method
+	 * then takes the class, and checks all the methods invoked by the leak against all the methods
+	 * of the class, and if any of them throw an exception, return true, requiring that string be rewritten.
+	 * 
+	 * @param insertion    the string that will be inserted as a sink/source that contains a method.
+	 * @author Kevin Cortright and Nicholas di Mauro
+	 *
+	 */
+	private boolean rewrite(String insertion) throws ClassNotFoundException {
+		String[] stringsInitial = insertion.split("=");
+		String newInsertion = stringsInitial[1];
+		insertion = newInsertion.replaceAll("\\s", "");
+		List<String> methods = new ArrayList<String>();
+		String canon_name = null;
+		String[] strings = insertion.split(".");
+		for (int i=0; i<strings.length; i++) {
+			if (strings[i].contains("()")) {
+				methods.add(strings[i]);
+			}
+			else {
+				canon_name+=strings[i];
+			}
+		}
+		for (int i=0; i<methods.size(); i++) {
+			methods.set(i, methods.get(i).substring(0, methods.get(i).indexOf("(")));
+		}
+		Class<?> c = Class.forName(canon_name);
+		Method[] allMethods = c.getMethods();
+		for (int i=0; i<allMethods.length; i++) {
+			for (int j=0; j<methods.size();j++) {
+				if (allMethods[i].toString().contains(methods.get(j))) {
+					if (allMethods[i].getExceptionTypes().length !=0) {
+						return true;
+					}
+				}
+			}
+		}
+		return false;
+		
 	}
 	
 	protected void addTryCatch(Statement statement) {
