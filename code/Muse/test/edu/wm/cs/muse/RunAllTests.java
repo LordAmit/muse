9ca6be78f811bbf89ef69e4fd227dc364501adc5
MichaelFoster <michaelrfoster0@gmail.com>
2020-10-15 14:40:12 -0400

    Created RunAllTests: JUnit suite of all existing tests (Fixed issue #17)
    Contains tests from the folders:
    	edu.wm.edu.cs.muse
    	edu.wm.edu.cs.muse.operatorsTest
    	edu.wm.edu.cs.muse.placementCheckerTest
    	edu.wm.edu.cs.muse.schemasTest

---
+++ code/Muse/test/edu/wm/cs/muse/RunAllTests.java
@@ -0,0 +1,30 @@
+package edu.wm.cs.muse;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import edu.wm.cs.muse.operatorsTest.*;
+import edu.wm.cs.muse.placementCheckerTest.PlacementCheckerTest;
+import edu.wm.cs.muse.schemasTest.*;
+
+@RunWith(Suite.class)
+@SuiteClasses({ 
+		MuseTest.class, // General Test
+		
+        ScopeSourceOperatorTest.class, // Operator Tests
+        TaintSinkOperatorTest.class,   // *
+        TaintSourceOperatorTest.class, // *
+        
+        PlacementCheckerTest.class, // PlacementChecker Test
+        
+        MuseComplexReachabilitySchemaTest.class, // Schema Tests
+        MuseReachabilitySchemaTest.class,        // *
+        ScopeSinkSchemaTest.class,               // *
+        ScopeSourceSchemaTest.class,             // *
+        TaintSinkSchemaTest.class,               // *
+        TaintSourceSchemaTest.class              // *
+        })
+public class RunAllTests {
+
+}
