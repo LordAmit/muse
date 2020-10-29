f8cd34a7aab9223126d1c603525b072546d7a503
MichaelFoster <michaelrfoster0@gmail.com>
2020-10-29 14:07:09 -0400

    Improved comments/readability of gui tests (Fixed #70)

--- code/Muse/test/edu/wm/cs/muse/guiTest/GuiTest.java
+++ code/Muse/test/edu/wm/cs/muse/guiTest/GuiTest.java
@@ -8,6 +8,8 @@ import edu.wm.cs.muse.dataleak.support.FileUtility;
 
 /**
  * Unit test file of  Muse Gui
+ * 
+ * Tests of the generateConfig function intentionally use hardcoded paths to test if it can correctly create a properties file given certain string inputs
  * @author Michael Foster
  *
  */
@@ -17,7 +19,7 @@ public class GuiTest {
 	File processedOutput;
 
 	@Test
-	public void only_required_fields() {
+	public void generateConfig_only_required_fields() {
 		expectedOutput = new File("test/output/sample_config_1.txt");
 		processedOutput = new File("testconfig.properties");
 		GenerateConfig.generateConfig("testconfig", "C:\\Users", "C:\\Users\\Admin\\muse\\mse\\code\\Muse\\Sample_Application\\app\\src", "SCOPESINK", false , "Sample App", "C:\\Users\\Admin\\Downloads\\test", false, "C:\\Users\\Admin\\Downloads\\insertion_log", "C:\\Users\\Admin\\Downloads\\execution_log", false, "sample_source_string", "sample_sink_string", "sample_varDec_string");
@@ -25,7 +27,7 @@ public class GuiTest {
 	}
 	
 	@Test
-	public void with_mutation_fields() {
+	public void generateConfig_with_mutation_fields() {
 		expectedOutput = new File("test/output/sample_config_2.txt");
 		processedOutput = new File("testconfig.properties");
 		GenerateConfig.generateConfig("testconfig", "C:\\Users", "C:\\Users\\Admin\\muse\\mse\\code\\Muse\\Sample_Application\\app\\src", "SCOPESINK", true , "Sample App", "C:\\Users\\Admin\\Downloads\\test", false, "C:\\Users\\Admin\\Downloads\\insertion_log", "C:\\Users\\Admin\\Downloads\\execution_log", false, "sample_source_string", "sample_sink_string", "sample_varDec_string");
@@ -33,7 +35,7 @@ public class GuiTest {
 	}
 	
 	@Test
-	public void with_log_analyze_fields() {
+	public void generateConfig_with_log_analyze_fields() {
 		expectedOutput = new File("test/output/sample_config_3.txt");
 		processedOutput = new File("testconfig.properties");
 		GenerateConfig.generateConfig("testconfig", "C:\\Users", "C:\\Users\\Admin\\muse\\mse\\code\\Muse\\Sample_Application\\app\\src", "SCOPESINK", false , "Sample App", "C:\\Users\\Admin\\Downloads\\test", true, "C:\\Users\\Admin\\Downloads\\insertion_log", "C:\\Users\\Admin\\Downloads\\execution_log", false, "sample_source_string", "sample_sink_string", "sample_varDec_string");
@@ -41,7 +43,7 @@ public class GuiTest {
 	}
 
 	@Test
-	public void with_custom_data_leak_fields() {
+	public void generateConfig_with_custom_data_leak_fields() {
 		expectedOutput = new File("test/output/sample_config_4.txt");
 		processedOutput = new File("testconfig.properties");
 		GenerateConfig.generateConfig("testconfig", "C:\\Users", "C:\\Users\\Admin\\muse\\mse\\code\\Muse\\Sample_Application\\app\\src", "SCOPESINK", false , "Sample App", "C:\\Users\\Admin\\Downloads\\test", false, "C:\\Users\\Admin\\Downloads\\insertion_log", "C:\\Users\\Admin\\Downloads\\execution_log", true, "sample_source_string", "sample_sink_string", "sample_varDec_string");
@@ -49,7 +51,7 @@ public class GuiTest {
 	}
 	
 	@Test
-	public void with_all_fields() {
+	public void generateConfig_with_all_fields() {
 		expectedOutput = new File("test/output/sample_config_5.txt");
 		processedOutput = new File("testconfig.properties");
 		GenerateConfig.generateConfig("testconfig", "C:\\Users", "C:\\Users\\Admin\\muse\\mse\\code\\Muse\\Sample_Application\\app\\src", "SCOPESINK", true , "Sample App", "C:\\Users\\Admin\\Downloads\\test", true, "C:\\Users\\Admin\\Downloads\\insertion_log", "C:\\Users\\Admin\\Downloads\\execution_log", true, "sample_source_string", "sample_sink_string", "sample_varDec_string");
